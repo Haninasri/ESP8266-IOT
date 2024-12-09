6e650673471ebe10319e77d618259af6ba9e559c
Haninasri <naasri.hani@gmail.com>
2024-12-09 19:38:01 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,13 @@ import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_apiKey,
-    authDomain: process.env.REACT_APP_authDomain,
+	authDomain: process.env.REACT_APP_authDomain,
 	databaseURL: "https://esp8266-a-8b851-default-rtdb.firebaseio.com",
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId: process.env.REACT_APP_appId,
-    measurementId: process.env.REACT_APP_measurementId
+	projectId: process.env.REACT_APP_projectId,
+	storageBucket: process.env.REACT_APP_storageBucket,
+	messagingSenderId: process.env.REACT_APP_messagingSenderId,
+	appId: process.env.REACT_APP_appId,
+	measurementId: process.env.REACT_APP_measurementId
   };
 
 // Initialize Firebase
